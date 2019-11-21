8dad1b7ff2d1b28edad5dbba1381dad8f843c039
Luis Fernando Cruz Carrillo <lfcruz@udes.edu.mx>
2019-11-21 09:36:10 -0600

    Based on Django

--- DragonScript/router.ts
+++ DragonScript/router.ts
@@ -1,7 +1,24 @@
 namespace ROUTER {
 
-    export function path() {
+    export function url_get_parser(url_get_parser_param_type: {}): [] {
+        let params: string[] = Object.keys(url_get_parser_param_type.parameter);
 
+        let url_arr = []
+
+        for (let param of params) {
+            if (param === 'pag') {
+                if (params.indexOf('/') >= 0) {
+                    url_arr = params.pag.split('/');
+                } else {
+                    url_arr.push(params.pag)
+                }
+                break;
+            }
+        }
+
+        return url_arr;
     }
+
+    export function
 
 }
