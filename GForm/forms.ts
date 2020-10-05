8fdc77b7c8067e1daa00151a904d308a3495aeef
Luis Fernando Cruz Carrillo <lfcruz@udes.edu.mx>
2020-10-05 14:15:16 -0500

    OK

--- GForm/forms.ts
+++ GForm/forms.ts
@@ -1,44 +1,49 @@
 
 namespace FORM {
 
-    export function conn(id_url: string) {
-        if (id_url.indexOf('https://docs.google.com/forms/')) {
-            return FormApp.openByUrl(id_url);
-        } else if (id_url) {
-            return FormApp.openByUrl(id_url);
+    export class Model {
+        form: GoogleAppsScript.Forms.Form
+
+        constructor(id_url: string) {
+            this.form = this.conn(id_url)
         }
 
-        return FormApp.getActiveForm();
-    }
-
-}
-
-export function Questions(form: GoogleAppsScript.Forms.Form, array_of?: string) {
-    // Obtiene las preguntas
-    let items = form.getItems()
-    let questions = []
-
-    for (let i = 0; i < items.length; i++) {
-        let item = items[i];
-        questions.push(
-            {
-                type: item.getType(),
-                id: item.getId(),
-                index: item.getIndex(),
-                title: item.getTitle(),
-                help_text: item.getHelpText(),
+        /**
+         * Connects to form
+         * @param id_url : URL or ID of Google Form
+         */
+        conn(id_url: string) {
+            if (id_url.indexOf('https://docs.google.com/forms/')) {
+                return FormApp.openByUrl(id_url);
+            } else if (id_url) {
+                return FormApp.openByUrl(id_url);
             }
-        );
+
+            return FormApp.getActiveForm();
+        }
+
+        infoQuestions() {
+            // Obtiene las preguntas
+            let items = this.form.getItems()
+            let questions = []
+
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i];
+                questions.push(
+                    {
+                        type: item.getType(),
+                        id: item.getId(),
+                        index: item.getIndex(),
+                        title: item.getTitle(),
+                        help_text: item.getHelpText(),
+                    }
+                );
+            }
+
+            return questions
+        }
+
     }
 
-    if (array_of) {
-        let list = []
-        for (const item of questions) {
-            list.push(item[array_of]);
-        }
-        return list;
-    } else {
-        return questions;
-    }
 
 }
